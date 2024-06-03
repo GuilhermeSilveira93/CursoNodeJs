e6d8b7b4f29d3620795ade230becd4c3d7df8d88
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2024-06-02 23:36:01 -0300

    g

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -10,7 +10,16 @@ class InverseNumber extends Transform {
 }
 
 const server = http.createServer(async (req, res) => {
-  return req.pipe(new InverseNumber()).pipe(res)
+  // juntando os pedacinhos para fazer algo depois
+  const buffers = []
+  for await (const chunk of req){
+    buffers.push(chunk)
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString()
+  console.log(fullStreamContent)
+  return res.end(fullStreamContent)
+  // return req.pipe(new InverseNumber()).pipe(res)
 })
 
 server.listen(3334)
